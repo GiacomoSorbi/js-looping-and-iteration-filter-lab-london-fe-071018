3d75f60b7aeeecb1ef86102ed2e42152ee334c0e
Giacomo Sorbi <g.sorbi@gmail.com>
2018-08-14 15:32:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Code your solution in this file
 const findMatching = (d,s) => d.filter(e=>e.toLowerCase() === s.toLowerCase());
-const findMatching
+const findMatching= (d,s) => d.filter(e=>e.startsWith(s));
